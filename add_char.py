011612a784caf8bafba25d8e79a52227748c2a30
Ji Young <lejiyo1020@gmail.com>
2022-04-29 02:52:47 +0900

    알파채널 이용한 이미지 합성

--- add_char.py
+++ add_char.py
@@ -4,13 +4,14 @@ import numpy as np
 # 뱃지 클래스
 class Character:
     def __init__(self):
-        me = cv2.imread('HouseZOOMClient/rabbit.png')
+        me = cv2.imread('HouseZOOMClient/rabbit.png',cv2.IMREAD_UNCHANGED)
         self.me = cv2.flip(me,1)
-        self.me = cv2.cvtColor(self.me, cv2.COLOR_BGRA2BGR)
-        self.gray = cv2.cvtColor(self.me, cv2.COLOR_BGR2GRAY)
-        _, self.mask_inv = cv2.threshold(self.gray, 0, 255, cv2.THRESH_BINARY_INV)
-        #_, self.mask = cv2.threshold(self.me[:,:,3], 1, 255, cv2.THRESH_BINARY)
-        #self.mask_inv = cv2.bitwise_not(self.mask)       
+        #self.me = cv2.cvtColor(self.me, cv2.COLOR_BGRA2BGR)
+        #self.gray = cv2.cvtColor(self.me, cv2.COLOR_BGR2GRAY)
+        #_, self.mask_inv = cv2.threshold(self.gray, 0, 255, cv2.THRESH_BINARY_INV)
+        _, self.mask = cv2.threshold(self.me[:,:,3], 1, 255, cv2.THRESH_BINARY)
+        self.mask_inv = cv2.bitwise_not(self.mask)   
+        self.me = cv2.cvtColor(self.me, cv2.COLOR_BGRA2BGR)    
         h, w, c = me.shape
         self.h = h
         self.w = w
@@ -25,13 +26,13 @@ class Character:
         
         roi = frame[x: x+self.h, y: y+self.w]
         
-        #masked_me = cv2.bitwise_and(self.me, self.me, None, mask=self.mask)
-        #masked_roi = cv2.bitwise_and(roi, roi, None, mask=self.mask_inv)
+        masked_me = cv2.bitwise_and(self.me, self.me, None, mask=self.mask)
+        masked_roi = cv2.bitwise_and(roi, roi, None, mask=self.mask_inv)
         
-        roi_char = cv2.add(self.me, roi, mask = self.mask_inv)
+        #roi_char = cv2.add(self.me, roi, mask = self.mask_inv)
         
-        result = cv2.add(roi_char, self.me)
-        #result = masked_me + masked_roi
+        #result = cv2.add(roi_char, self.me)
+        result = masked_me + masked_roi
 
         frame[x: x+self.h, y: y+self.w] = result
 
