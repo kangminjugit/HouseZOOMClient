798ba5f02fe6ae1cfb94d426b073cc7aa18b71b1
Ji Young <lejiyo1020@gmail.com>
2022-05-26 12:13:09 +0900

    sleep detect, add alarm

--- face_Detect.py
+++ face_Detect.py
@@ -1,5 +1,7 @@
 import cv2
 import math
+import timeit
+import pygame
 import mediapipe as mp
 from add_image import Bomb
 
@@ -8,6 +10,15 @@ class face_detecter:
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.mp_face_mesh = mp.solutions.face_mesh
+        
+        self.TIMER_FLAG = False #Flag to activate 'start_closing' variable, which measures the eyes closing time.
+        self.ALARM_FLAG = False #Flag to check if alarm has ever been triggered.
+        self.start_closing = timeit.default_timer()
+        self.COUNTER = 0
+        self.ALARM_COUNT = 0 #Number of times the total alarm rang.
+        self.RUNNING_TIME = 0
+        self.PREV_TERM = 0 #Variable to measure the time eyes were being opened until the alarm rang.
+
         
         # Left eyes indices 
         self.LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
@@ -31,13 +42,56 @@ class face_detecter:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             
         if results.multi_face_landmarks:
+            
+            #얼굴 인식하고 있을 때 눈 감지
             mesh_coords = self.landmarksDetection(image, results, False)
             ratio = self.blinkRatio(image, mesh_coords, self.RIGHT_EYE, self.LEFT_EYE)
             #cv2.putText(image, f'ratio {ratio}', (100, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
- 
-            if ratio >5.5:
-                #CEF_COUNTER +=1
-                cv2.putText(image, 'Blink', (200, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+
+            if ratio > 5.5:
+                if not self.TIMER_FLAG:
+                    self.start_closing = timeit.default_timer()
+                    self.TIMER_FLAG = True
+                self.COUNTER += 1
+
+                if self.COUNTER >= 150:
+                    image = self.draw_bomb(image)
+                    mid_closing = timeit.default_timer()
+                    closing_time = round((mid_closing-self.start_closing),3)
+
+                    if closing_time >= self.RUNNING_TIME:
+                        if self.RUNNING_TIME == 0 :
+                            CUR_TERM = timeit.default_timer()
+                            OPENED_EYES_TIME = round((CUR_TERM - self.PREV_TERM),3)
+                            self.PREV_TERM = CUR_TERM
+                            self.RUNNING_TIME = 1.75
+
+                        self.RUNNING_TIME += 5
+                        self.ALARM_FLAG = True
+                        self.ALARM_COUNT += 1
+
+                        self.sound_alarm()
+
+                #cv2.putText(image, 'Blink', (200, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+            
+            else:
+                self.COUNTER = 0
+                self.TIMER_FLAG = False
+                self.RUNNING_TIME = 0
+                self.ALARM_FLAG = False
+
+            
+            """   
+            #얼굴 눈 윤곽선 그리기
+            for face_landmarks in results.multi_face_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=self.mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=self.mp_drawing_styles
+                    .get_default_face_mesh_contours_style()) 
+            """           
                       
         else:
             #뱃지 그리기
@@ -46,15 +100,24 @@ class face_detecter:
             image = cv2.flip(image, 1)  
             
         image = cv2.flip(image, 1)          
-        return(image) 
+        return(image)
+    
+    def draw_bomb(self,image):
+        image = cv2.flip(image, 1)  
+        image = self.bomb.add_bomb(image)
+        image = cv2.flip(image, 1)
+        return image
+    
+    def sound_alarm(self) :
+        pygame.mixer.init()
+        pygame.mixer.music.load('HouseZOOMClient\knock.MP3')
+        pygame.mixer.music.play()
+                
     
     def landmarksDetection(self, img, results, draw=False):
         img_height, img_width= img.shape[:2]
         # list[(x,y), (x,y)....]
         mesh_coord = [(int(point.x * img_width), int(point.y * img_height)) for point in results.multi_face_landmarks[0].landmark]
-        #if draw :
-            #[cv.circle(img, p, 2, (0,255,0), -1) for p in mesh_coord]
-
         # returning the list of tuples for each landmarks 
         return mesh_coord
     
@@ -83,14 +146,16 @@ class face_detecter:
         lv_bottom = landmarks[left_indices[4]]
         
         # Finding Distance Right Eye
-        rhDistance = self.euclaideanDistance(rh_right, rh_left)
+        rhDistance = self.euclaideanDistance(rh_right, rh_left) 
         rvDistance = self.euclaideanDistance(rv_top, rv_bottom)
+        #cv2.putText(img, f'rvDistance{rvDistance}', (100, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+        #cv2.putText(img, f'rhDistance{rhDistance}', (100, 150), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
         
         # Finding Distance Left Eye
         lvDistance = self.euclaideanDistance(lv_top, lv_bottom)
         lhDistance = self.euclaideanDistance(lh_right, lh_left)
         # Finding ratio of LEFT and Right Eyes
-        reRatio = rhDistance/rvDistance
-        leRatio = lhDistance/lvDistance
+        reRatio = rhDistance/(rvDistance+0.1)
+        leRatio = lhDistance/(lvDistance+0.1)
         ratio = (reRatio+leRatio)/2
         return ratio
